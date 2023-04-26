47fbad91a1ab4b5759bcfdc9a791933d6616e6f7
dzz_cat <dzz_cat@mail.ru>
2023-04-26 13:19:12 +0400

    add hw

---
+++ 34/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+//  1
+
+Console.WriteLine("Задайте количество элементов в массиве:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Результат: "); // вывод массива
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+
+
+// 2
+
+// int size() // Длинна массива
+// {
+//     Console.WriteLine("Задайте количество элементов в массиве: ");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     return size;
+// }
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+// }
+
+// int Count(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] % 2 == 0) count++;
+//     }
+//     return count;
+// }
+
+// int[] array = new int[size()];
+// FillArray(array);
+// Console.WriteLine($"Количество чётных чисел в массиве: {Count(array)}");
